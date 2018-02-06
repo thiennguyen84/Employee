98fe4bb7fbc67e85a8022820510fd8f36298a8ca
thiennh <thiennh@haposoft.com>
2018-02-06 11:53:53 +0700

    upload project

---
+++ app/Employee.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+use App\Employee;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    //
+    protected $table = 'employees';
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Quản lí nhân viên</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container">
+		<h1>Trang chủ</h1>
+	</div>
+
+</body>
+</html>
